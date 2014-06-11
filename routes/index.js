b15c5d9883ca761a4e3dce413d22b0b6b49a4e2b
stanley g <sgirshik@gmail.com>
2014-06-11 00:53:23 +0000

    my working mongoose commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
+var User = require('../models/User');
+var mongoose = require('mongoose');
+// connecto to mongo
+mongoose.connect('mongodb://localhost/BuzzerTest');
+
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
+
+/* POST register */
+router.post('/register' , function(req,res){
+         var user = new User( {
+           name: req.body,
+           email: req.body.email 
+         });
+        console.log("checkpoint1");
+        user.save(function(err){
+         if(err) {console.log("Error inserting user" , err);next();}
+          res.render ('members',{title: 'Members Home'});
+        });
+});
+
 
 module.exports = router;
